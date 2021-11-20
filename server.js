8889ab9befbc21dd29854a2b8f9da68289350cb6
eren-ozdemir <eren2624@gmail.com>
2021-11-21 01:09:39 +0300

    .

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const ChatLog = require("./models/ChatLog");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 //User Router
 const usersRouter = require("./routes/usersRoute");
